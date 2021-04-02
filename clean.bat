94bb5d59ea762f70b6c0ae236514f16530208b17
depler <depler.mv@gmail.com>
2021-04-02 12:26:17 +0300

    Add project files.

---
+++ clean.bat
@@ -0,0 +1,2 @@
+rmdir /s /q .vs
+rmdir /s /q x64
---
+++ code/version.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define PEERID_PREFIX             "-TR3000-"
+#define USERAGENT_PREFIX          "3.00"
+#define VCS_REVISION              "bb6b5a062e"
+#define VCS_REVISION_NUM          bb6b5a062e
+#define SHORT_VERSION_STRING      "3.00"
+#define LONG_VERSION_STRING       "3.00 (bb6b5a062e)"
+#define VERSION_STRING_INFOPLIST  3.00
+#define BUILD_STRING_INFOPLIST    14714.3.00
+#define MAJOR_VERSION             3
+#define MINOR_VERSION             00
+
+#define TR_STABLE_RELEASE 1
