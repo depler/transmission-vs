01bd2e7699d485646a4b158aeed843ff84bde9b1
depler <depler.mv@gmail.com>
2022-08-09 13:00:48 +0300

    code

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -3,6 +3,6 @@
 #define PEERID_PREFIX             "-TR4000-"
 #define USERAGENT_PREFIX          "4.00"
 #define SHORT_VERSION_STRING      "4.00"
-#define LONG_VERSION_STRING       "4.00 (beta)"
+#define LONG_VERSION_STRING       "4.00 beta (build 2022-08-09)"
 
 #define TR_STABLE_RELEASE 1
