37835c67b69b2d19f1c77a89fb1a2dd05f15371d
depler <depler.mv@gmail.com>
2022-08-31 12:02:47 +0300

    code

--- libtransmission/version.h
+++ libtransmission/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define BUILD_DATE                "2022-08-23"
+#define BUILD_DATE                "2022-08-31"
 #define PEERID_PREFIX             "-TR4000-"
 #define USERAGENT_PREFIX          "4.00"
 #define SHORT_VERSION_STRING      "4.00"
